9d65d0dc7c0d782dacfb9a93fb07155c6a6a3941
Anna Berg <anbj21@student.bth.se>
2022-12-20 19:56:00 +0100

    Change prices

--- src/test/components/map.test.js
+++ src/test/components/map.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, act, wait } from '@testing-library/react';
-import MapCity from '../../components/admin/map';
+import MapCity from '../../components/pages/admin/handleBikes/components/map';
 
 test('renders MapCity component', async () => {
   let center = { lat: 0, lng: 0 };
